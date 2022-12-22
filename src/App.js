460e3968b6e8af5849c2f5151ef0669783325b10
Jonathan Koh <jonathan.koh75@gmail.com>
2022-12-22 17:34:17 +0800

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>jonathan typed this</h1>
+        <h1>React App Hosted on Github Pages</h1>
+        <a href="https://blog.logrocket.com/deploying-react-apps-github-pages/">https://blog.logrocket.com/deploying-react-apps-github-pages/</a>
+        <p>Everytime a change is made, you need to run these commands</p>
+        <ul>
+          <li>git add .</li>
+          <li>git commit -m "setup gh-pages"</li>
+          <li>git push</li>
+          <li>npm run deploy</li>
+        </ul>
+        <p>and it will make its changes on github pages</p>
     </div>
   );
 }
